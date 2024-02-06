b8a60ee778cebc4fd20a63362aaf58a86e255ed4
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-06 09:12:46 +0500

    Update ArrayTasks.java

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -11,7 +11,7 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-
+        String[] seasons = {"winter", "spring", "summer", "autumn"};
     }
 
     /**
@@ -25,7 +25,11 @@ public class ArrayTasks {
      * length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-
+        int[] numbers = new int[length];
+        for(int i=0; i<length; i++){
+            numbers[i]=i+;
+        }
+        return numbers;
     }
 
     /**
@@ -37,7 +41,11 @@ public class ArrayTasks {
      * arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-
+        int sum=0;
+           for(int i=0; i<arr.length; i++){ 
+               sum+=arr[i];
+           }
+        return sum;
     }
 
     /**
