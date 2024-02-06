9b9adaf6e7132bfff2fd0e220b451ae9ffe95052
Davron-Adbusalomov <115224119+Davron-Adbusalomov@users.noreply.github.com>
2024-02-06 09:30:56 +0500

    Update AlgorithmComplexity.java

--- src/main/java/com/epam/mjc/stage0/AlgorithmComplexity.java
+++ src/main/java/com/epam/mjc/stage0/AlgorithmComplexity.java
@@ -5,10 +5,10 @@ import com.epam.mjc.stage0.utils.Complexity;
 /**
  * Tasks for assessing the algorithm complexity.
  * The following are the ratios of the constants with the BigO estimate:
- * <p>
+ * 
  * Complexity.O_LOG_N       -> O(log N)
- * Complexity.O_N_2         -> O(log N^2)
- * Complexity.O_FACTORIAL_N -> O(!N)
+ * Complexity.O_N_2         -> O(N^2)
+ * Complexity.O_FACTORIAL_N -> O(N!)
  * Complexity.O_2_N         -> O(2 * N)
  * Complexity.O_N_LOG_N     -> O(N * log N)
  * Complexity.O_1           -> O(1)
@@ -19,28 +19,28 @@ public class AlgorithmComplexity {
     /**
      * Return the linear search complexity if the element you are looking for is at the end of the array.
      */
-    public Complexity badLinerSearch() {
-        return O(1);
+    public Complexity badLinearSearch() {
+        return Complexity.O_1;
     }
 
     /**
      * Return the complexity of accessing an array element by index.
      */
     public Complexity arrayIndexItemAccess() {
-        return O(1);
+        return Complexity.O_1;
     }
 
     /**
      * Return the complexity of a binary search.
      */
     public Complexity binarySorting() {
-        return O(logn);
+        return Complexity.O_LOG_N;
     }
 
     /**
      * Return the complexity of the twoCycleSorting algorithm.
      */
     public Complexity twoCycleSorting() {
-        return O(n^2);
+        return Complexity.O_N_2;
     }
 }
